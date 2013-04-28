c2f5f4fcf1d12a4622a6cedd4d0c2fcf62c3072e
SuperLight2 <superlight89@gmail.com>
2013-04-28 22:50:55 +0400

    fix replace columns

--- tools/columns_replacer.py
+++ tools/columns_replacer.py
@@ -9,9 +9,15 @@ _logger = logging.getLogger(__name__)
 def replace_columns(result_filepath, replacing_filepath, replace_columns, dindex = 0):
     _logger.debug("Result file: %s" % result_filepath)
     _logger.debug("Replacing file: %s" % replacing_filepath)
+    filepath_copy = "copy.tmp.file" + replacing_filepath[-3:]
+    filepath_new = "new.tmp.file" + replacing_filepath[-3:]
+    _logger.debug("TMP copy file: %s" % filepath_copy)
+    _logger.debug("TMP new file: %s" % filepath_new)
+    os.system("cp %s %s" % (replacing_filepath, filepath_copy))
+
     for filepath, column_id, not_mathed_value in replace_columns:
-        _logger.debug("Replacing Column id = %d" % column_id)
-        _logger.debug("Path to mapping file= %s" % filepath)
+        _logger.debug("Replacing Column id: %d" % column_id)
+        _logger.debug("Path to mapping file: %s" % filepath)
         d = {}
         for line in SmartReader().open(filepath):
             s = line.strip().split('\t')
@@ -21,11 +27,6 @@ def replace_columns(result_filepath, replacing_filepath, replace_columns, dindex
         not_mathed_ids = set()
         not_mathed_lines = 0
 
-        filepath_copy = "copy.tmp.file" + replacing_filepath[-3:]
-        filepath_new = "new.tmp.file" + replacing_filepath[-3:]
-        _logger.debug("TMP copy file: %s" % filepath_copy)
-        _logger.debug("TMP new file: %s" % filepath_new)
-        os.system("cp %s %s" % (replacing_filepath, filepath_copy))
         with SmartWriter().open(filepath_new) as file_new:
             for line in SmartReader().open(filepath_copy):
                 s = line.strip().split('\t')
@@ -40,6 +41,6 @@ def replace_columns(result_filepath, replacing_filepath, replace_columns, dindex
                 print >> file_new, "\t".join(map(str, s))
             file_new.close()
         os.system("mv %s %s" % (filepath_new, filepath_copy))
-        _logger.debug("Not matched uniq ids = %d" % len(not_mathed_ids))
-        _logger.debug("Not matched lines = %d" % not_mathed_lines)
+        _logger.debug("Not matched uniq ids: %d" % len(not_mathed_ids))
+        _logger.debug("Not matched lines: %d" % not_mathed_lines)
     os.system("mv %s %s" % (filepath_copy, result_filepath))
