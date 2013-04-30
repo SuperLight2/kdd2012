7600380c729602ffca442b2cfda669441f256447
SuperLight2 <superlight89@gmail.com>
2013-04-30 13:19:45 +0400

    add features click and impression

--- dolbilka.py
+++ dolbilka.py
@@ -1,9 +1,36 @@
 from optparse import OptionParser
-import logging
-from tools.user_reader import InstanceReader
-from tools.smart_writer import SmartWriter
+import os
+import time
 
+import logging
 _logger = logging.getLogger(__name__)
+
+
+def shell_cmd(cmd):
+    _logger.info(cmd)
+    os.system(cmd)
+
+
+def join_all_files(result_file, files_to_join):
+    tmp_filepath = "dirty_tmp_file.never_used_2432"
+    shell_cmd("rm -f %s; cp %s %s" % (result_file, files_to_join[0], result_file))
+    for filepath in files_to_join[1:]:
+        _logger.info("Joining %s" % filepath)
+        os.system("paste %s %s > %s; mv %s %s" % (result_file, filepath, tmp_filepath, tmp_filepath, result_file))
+    os.system("rm -f %s" % tmp_filepath)
+
+
+def calc_script(result_file, script_filepath, data_file, arguments = None):
+    _logger.info("Running %s script..." % script_filepath)
+    os.system("rm -f %s" % result_file)
+    if script_filepath.endswith(".py"):
+        script_filepath = "python " + script_filepath
+    if arguments is None:
+        arguments = []
+    running_time = time()
+    shell_cmd("%s %s %s > %s" % (script_filepath, data_file, " ".join(arguments), result_file))
+    _logger.info("Running time: %s sec" % str(time() - running_time))
+    return result_file
 
 
 def main():
@@ -16,10 +43,8 @@ def main():
     training_filepath = args[0]
     test_filepath = args[1]
 
-    _logger.info("Generating output column")
-    with  SmartWriter().open("answer.tsv") as fout:
-        for instance in InstanceReader().open(training_filepath):
-            print >> fout, instance.clicks, instance.impressions
+    calc_script("train_" + "features_click_and_impression.tsv", "features_click_and_impression.py", training_filepath)
+    calc_script("train_" + "features_click_and_impression.tsv", "features_click_and_impression.py", training_filepath)
 
 
 if __name__ == '__main__':
