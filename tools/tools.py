22e083fc56fb41df050f5252c23485c01aa95110
SuperLight2 <superlight89@gmail.com>
2013-05-03 01:29:01 +0400

    fix tokenator calcer

--- tools/tools.py
+++ tools/tools.py
@@ -21,6 +21,8 @@ def cosine(vector1, vector2):
     result = 0
     for i in xrange(len(vector1)):
         result += vector1[i] * vector2[i]
+    if abs(result) < 1e-30:
+        return 0
     result /= norm(vector1)
     result /= norm(vector2)
     return result
